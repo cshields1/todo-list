65ad618b7e9c0255a149b43b7726c302c6e51436
Craig <84283410+cshields1@users.noreply.github.com>
2021-11-07 17:05:40 -0800

    Start DOM logic

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// to-do objects
+// title, description, dueDate, priority, notes
+// projects - collections of to-do objects
+// default project and able to create new ones
+// app logic (create, mark complete, add/change note, change priority/dueDate)
+// DOM logic
+// interface: view all projects, view all to-dos in project (title/dueDate, priority indicator), expand single to-do to view/edit details, delete to-do
+// use localStorage to save projects and to-dos
+
+const loadApp = (() => {
+
+    const toDoItems = [];
+
+    
+
+    const body = document.body;
+
+    const toDoWindow = document.createElement('section');
+    toDoWindow.classList.add('window');
+
+})();
